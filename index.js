38734105c7a86946d5a73b571cc74cb6c7f190ae
Tee-Mothiba <Tee.Mothiba@gmail.com>
2023-03-11 04:16:15 +0200

    First Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const oldh2 = document.querySelector('h2');
+
+oldh2.setAttribute('#', newly-created);
+const newh3 = document.createElement('h3');
+oldh2.left-top-container.replaceChild(newh3,oldh2);
+
+console.log(oldh2);
+
+
+
+
+
